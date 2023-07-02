458f25b229430ee53f0a55215b0cdae2313c0bef
DeetSteve00 <98794456+DeetSteve00@users.noreply.github.com>
2023-07-02 11:51:20 -0600

    Add files via upload

--- ASCIIDraw/Main.cpp
+++ ASCIIDraw/Main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <conio.h>
-#include <windows.h> /* Bet it will only work on Windows 10+ */
+#include <windows.h>
 #include "engin.h"
 
 /**
@@ -9,21 +9,21 @@
  * This is a small program to create ASCII art using a cursor and you keyboard
  * You can also add colors to your canvas if you feel creative
  * 
- * This small program is now fully finished, bugs are impossible, everything is meant to be what it is
- * There should and isn't any unexpected behaivour on this small program, there's just no way
+ * This small program is not fully finished, bugs are possible, everytihng is meant to be what it is,
+ * there should and isn't any unexpected behaivour on this small program,
  * Given the same input, there will be the same output
 **/
 
 const int Width = 120, Height = 30; /* Every console program starts with default 120 x 30 screen */
 int cursorX = (Width/2)-1, cursorY = Height/2;
-Display display(0x07, "ASCIIDraw v0.1", true);
+Display display(0x07, "ASCIIDraw v0.2", true);
 #define MAP(X) Map_ ## X
 	UCHAR Map_Char[Width*Height];
 	byte Map_Color[Width*Height];
 FILE* file;
 
 CHAR Input[100] = "XXX"; /* Three digit number 0 - 255 + terminating char */
-
+CHAR CopiedChar = ' ';
 int i, a, b;
 
 int main(int argc, char const *argv[]) {
@@ -48,7 +48,7 @@ int main(int argc, char const *argv[]) {
 		}
 
 		display.srcDisplay[display.in(cursorX, cursorY)].Attributes = 0x1f;
-		display.WriteOutput(); // game chrashes here
+		display.WriteOutput();
 
 		if(_kbhit()) switch(_getch()) { /* Just about everything happens here */
 			case 27: exit(0); break;
@@ -83,19 +83,22 @@ int main(int argc, char const *argv[]) {
 					Input[i] = a;
 					i++;
 				}
-				MAP(Color)[cursorY*Width+cursorX] = (CHAR) strtol(Input, NULL, 16); /* Convert Hexadecimal string to integer */
+				MAP(Color)[cursorY*Width+cursorX] = (CHAR) strtol(Input, NULL, 16); /* Convert Hex string to integer */
 				break;
 
-			case 'o': for(i = 0; i < Width*Height; i++) MAP(Color)[i] = 0xf0; break;
-			case 'p': for(i = 0; i < Width*Height; i++) MAP(Color)[i] = 0x0f; break;
+			case 'o': CopiedChar = MAP(Char)[cursorY*Width+cursorX]; break; /* Copy */
+			case 'p': MAP(Char)[cursorY*Width+cursorX] = CopiedChar; break; /* Paste */
 
-			case 'm': /* Save ascii art on "out.bin" */
-				file = fopen("out.bin", "w");
-				a = 0, b = 0;
+			case 'z': for(i = 0; i < Width*Height; i++) MAP(Color)[i] = 0xf0; break;
+			case 'x': for(i = 0; i < Width*Height; i++) MAP(Color)[i] = 0x0f; break;
+
+			case 'm': /* Save ascii art on "out.ASCIIArt" */
+				file = fopen("out.aart", "w");
+				a = 0;/*, b = 0;*/
 				while (a < Width*Height) {
 					fputc(MAP(Char)[a], file);
-					fputc(MAP(Color)[b], file);
-					a++, b++;
+					fputc(MAP(Color)[a], file);
+					a++;
 				}
 				fclose(file);
 				break;
